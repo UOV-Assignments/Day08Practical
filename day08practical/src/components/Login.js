54b810a5172e0f30054b6e53de4093ec08f649c9
Lasanga Dissanayaka <129869155+lasanga890@users.noreply.github.com>
2024-12-01 09:05:49 +0000

    Create login component basically

--- day08practical/src/components/Login.js
+++ day08practical/src/components/Login.js
@@ -1,7 +1,26 @@
+import { useState } from "react"
+import Button from 'react-bootstrap/Button';
 export default function Login(params) {
+    const [username,setUsername] = useState('');
+    const [password,setPassword] = useState('')
     return(
         <>
-        <div>Login</div>
+        <div>
+            <h1>Login</h1>
+            <table align="center">
+                <tbody>
+                    <tr  align="center">
+                        <td>Username:<input type="text" name="username" value={username} onChange={(e)=>{setUsername(e.target.value)}} /></td>
+                    </tr>
+                    <tr  align="center">
+                        <td>Password:<input type="password" name="password" value={password} onChange={(e)=>setPassword(e.target.value)}/></td>
+                    </tr>
+                    <tr>
+                        <td><Button as="input" type="submit" value="Submit" /></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
         </>
     )
 }
