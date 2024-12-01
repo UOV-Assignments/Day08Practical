1284b227b4626365f16ba1d9d9521600465df79c
Lasanga Dissanayaka <129869155+lasanga890@users.noreply.github.com>
2024-12-01 09:16:19 +0000

    "Added React Router and routes for Login and Dashboard components"

--- day08practical/src/App.js
+++ day08practical/src/App.js
@@ -1,12 +1,20 @@
 
 import './App.css';
+import { BrowserRouter as Router, Route,Routes } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Login from './components/Login';
+import Dashboard from './components/Dashboard';
 
 function App() {
   return (
     <div className="App">
-      <Login />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path='/dashboard' element={<Dashboard />} />
+          
+        </Routes>
+      </Router>
     </div>
   );
 }
