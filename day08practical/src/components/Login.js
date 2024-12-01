daf47c9ddfc39f5f0eaaf205c2924e5d6f1e00c9
Lasanga Dissanayaka <129869155+lasanga890@users.noreply.github.com>
2024-12-01 14:00:44 +0000

    "Updated Login component to include navigation and login validation"

--- day08practical/src/components/Login.js
+++ day08practical/src/components/Login.js
@@ -1,26 +1,67 @@
-import { useState } from "react"
-import Button from 'react-bootstrap/Button';
-export default function Login(params) {
-    const [username,setUsername] = useState('');
-    const [password,setPassword] = useState('')
-    return(
-        <>
-        <div>
-            <h1>Login</h1>
-            <table align="center">
-                <tbody>
-                    <tr  align="center">
-                        <td>Username:<input type="text" name="username" value={username} onChange={(e)=>{setUsername(e.target.value)}} /></td>
-                    </tr>
-                    <tr  align="center">
-                        <td>Password:<input type="password" name="password" value={password} onChange={(e)=>setPassword(e.target.value)}/></td>
-                    </tr>
-                    <tr>
-                        <td><Button as="input" type="submit" value="Submit" /></td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
-        </>
-    )
+import { useState } from "react";
+import Button from "react-bootstrap/Button";
+import { useNavigate } from "react-router-dom";
+export default function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const Uname = "admin";
+
+  const Upwd = "admin";
+
+  const handleSubmit = () => {
+    if (Uname === username && Upwd === password) {
+      alert("Login Successfully");
+      navigate("/dashbord");
+    } else {
+      alert("Login Failure");
+    }
+  };
+
+  return (
+    <>
+      <div>
+        <h1>Login</h1>
+        <table align="center">
+          <tbody>
+            <tr align="center">
+              <td>
+                Username:
+                <input
+                  type="text"
+                  name="username"
+                  value={username}
+                  onChange={(e) => {
+                    setUsername(e.target.value);
+                  }}
+                />
+              </td>
+            </tr>
+            <tr align="center">
+              <td>
+                Password:
+                <input
+                  type="password"
+                  name="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <Button
+                  as="input"
+                  type="submit"
+                  value="Submit"
+                  onClick={handleSubmit}
+                />
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
 }
